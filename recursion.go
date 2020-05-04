d79592f77e0e0deef65a90172e7bf3a8035bef86
dimasmemas <dimichkit@gmail.com>
2020-05-04 13:13:43 +0300

    Added recursive sum implementation

--- recursion.go
+++ recursion.go
@@ -13,8 +13,17 @@ func sum(nums []int) int {
 }
 
 
+func recursiveSum(nums []int) int {
+  if len(nums) == 0 {
+    return 0
+  }
+  return nums[0] + recursiveSum(nums[1:])
+}
+
+
 func main() {
   array := []int{2, 42, -23, 3, 32,10}
 
   fmt.Println("Sum of", array, "is:", sum(array))
+  fmt.Println("Recursive sum of", array, "is:", recursiveSum(array))
 }
