7d99eddbb3439e47856cb13b60072d1c3374e3f1
dimasmemas <dimichkit@gmail.com>
2020-05-14 13:39:41 +0300

    Added intersection implementation

--- greedy.go
+++ greedy.go
@@ -16,9 +16,46 @@ func inititalizeSet(items []string) Set {
 }
 
 
+func itemInSet(item string, set Set) bool {
+  for key := range set {
+    if key == item {
+      return true
+    }
+  }
+  return false
+}
+
+
+func intersection(set1 Set, set2 Set) Set{
+    var intersectionSet = Set{}
+
+    for item := range set1 {
+      if itemInSet(item, set2) {
+        intersectionSet[item] = true
+      }
+    }
+
+    return intersectionSet
+}
+
+
 func main() {
     statesList := []string{"mt", "wa", "or", "id", "nv", "ut", "ca", "az"}
     statesNeeded := inititalizeSet(statesList)
 
     fmt.Println(statesNeeded)
+
+    var stations = map[string]Set{}
+    stations["kone"] = inititalizeSet([]string{"id", "nv", "ut"})
+    stations["ktwo"] = inititalizeSet([]string{"wa", "id", "mt"})
+    stations["kthree"] = inititalizeSet([]string{"or", "nv", "ca"})
+    stations["kfour"] = inititalizeSet([]string{"nv", "ut"})
+    stations["kfive"] = inititalizeSet([]string{"ca", "az"})
+
+    fmt.Println(stations)
+
+    var finalStations = Set{}
+    fmt.Println(finalStations)
+
+    fmt.Println(intersection(stations["kone"], stations["kfour"]))
 }
